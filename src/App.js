92652bb68c16499a9b08467850d9b3c50ce8d3a0
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-04-01 21:45:39 +0300

    1.3

--- src/App.js
+++ src/App.js
@@ -12,8 +12,13 @@ function App() {
     <header>
         <h1><b>Albert's To Do List</b></h1>
     </header>
-    <Form todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-    <TodoList/>
+    <Form 
+    inputText={inputText} 
+    todos={todos} 
+    setTodos={setTodos} 
+    setInputText={setInputText}
+    />
+    <TodoList setTodos={setTodos} todos={todos}/>
 
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const Todo = () => {
+const Todo = ({text, todos,setTodos}) => { 
+  //Events
+  const deleteHandler =() =>{
+    setTodos(todos.filter((el)=> el.id !== Todo.id));
+  }
   return (
     <div className='todo'>
-        <li className='todo-item'>Hey</li>
+        <li className='todo-item'>{text}</li>
         <button className='complete-btn'>
             <i className='fas fa-check'></i>
         </button>
-        <button className="trash-btn">
+        <button onClick={deleteHandler} className="trash-btn">
             <i className="fas fa-trash"></i>
         </button>
     </div>
